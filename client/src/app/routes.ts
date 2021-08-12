ef02b9cac8a57527df898e9bcdc41b86077fdab0
Prasadraj6897 <prasadraj32895@gmail.com>
2021-08-12 22:35:27 +0530

    Created RouterLink in html file

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { SignupComponent } from './user/signup/signup.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const appRoutes: Routes = [
     {
@@ -8,6 +10,13 @@ export const appRoutes: Routes = [
         children: [{ path: '', component: SignupComponent }]
     },
     {
-        path: '', redirectTo: '/signup', pathMatch: 'full' //for deafault we are redirecting
+        path: 'login', component: UserComponent,
+        children: [{ path: '', component: SignInComponent }]
+    },
+    {
+        path: 'userprofile', component: UserProfileComponent
+    },
+    {
+        path: '', redirectTo: '/login', pathMatch: 'full'
     }
 ];
