8ade45d26825519917874bf11601be961edf0403
Prasadraj6897 <prasadraj32895@gmail.com>
2021-08-12 18:45:22 +0530

    Created validation in signup form

--- client/src/app/shared/user.service.ts
+++ client/src/app/shared/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { User } from './user.model';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
--- client/src/app/user/signup/signup.component.ts
+++ client/src/app/user/signup/signup.component.ts
@@ -10,10 +10,15 @@ import { UserService } from '../../shared/user.service'
   providers: [UserService]
 })
 export class SignupComponent implements OnInit {
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  showSucessMessage: boolean;
+  serverErrorMessages: string;
 
   constructor(public userService: UserService) { }
 
   ngOnInit(): void {
   }
+
+  
 
 }
